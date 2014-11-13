53a3c89bb5e1a0e56258eb3a94ad2bb2c9f44fe4
Aaron Bieber <deftly@gmail.com>
2014-11-13 14:22:53 -0700

    initial

---
+++ geo.js
@@ -0,0 +1,22 @@
+var http = require('http'),
+geo = require('geoip'),
+City = geo.City,
+city = new City("/var/db/GeoLiteCity.dat"),
+server = http.createServer(function(req, res) {
+    var from = req.connection.remoteAddress || req.headers['x-forwarded-for'] || 'unknown';
+    if (from === '127.0.0.1') {
+	from = req.headers['x-forwarded-for'];
+    }
+    var cobj = city.lookup(from, function(err, data) {
+	if (from === 'unknown' || err) {
+	    console.log(err);
+	    res.end(from);
+	    return;
+	}
+	if (req.url.match('json')) {
+	    res.end(JSON.stringify(data));
+	} else {
+	    res.end(data.latitude + ", " + data.longitude);
+	}
+    });
+}).listen(3099);
